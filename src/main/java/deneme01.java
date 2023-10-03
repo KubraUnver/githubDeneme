db04e4595331952941ccf2be6199f9a70f516d80
KübraÜnver <kubraunver21@gmail.com>
2023-10-03 23:39:29 +0300

    ikinci

--- src/main/java/deneme01.java
+++ src/main/java/deneme01.java
@@ -2,5 +2,7 @@ public class deneme01 {
     public static void main(String[] args) {
 
         System.out.println("gitHub");
+        System.out.println("gitHub");
+        System.out.println("gitHub");
     }
 }
